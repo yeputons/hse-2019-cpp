d899464d7eefd464c8ddd03305e0fbd52833e19c
Egor Suvorov <egor.suvorov@gmail.com>
2020-04-18 13:22:38 +0300

    26-200421/plan: add more details

---
+++ 26-200421/code/rtti-boost-name.cpp
@@ -0,0 +1,15 @@
+#include <boost/core/demangle.hpp>
+#include <typeinfo>
+#include <iostream>
+
+template<class T> struct X
+{
+};
+
+int main()
+{
+    char const * name = typeid(X<int>).name();
+
+    std::cout << name << std::endl; // prints 1XIiE
+    std::cout << boost::core::demangle( name ) << std::endl; // prints X<int>
+}

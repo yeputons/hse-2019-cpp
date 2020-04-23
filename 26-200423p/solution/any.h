ac1bfe34bf5c57094691873153b5b30555658d19
Egor Suvorov <egor.suvorov@gmail.com>
2020-04-23 11:21:39 +0300

    26-200423p/solution: make any_data_base abstract

--- 26-200423p/solution/any.h
+++ 26-200423p/solution/any.h
@@ -45,10 +45,10 @@ struct any {
 private:
     struct any_data_base {
         virtual ~any_data_base() {}
-        virtual const std::type_info& type() const noexcept;
-        virtual std::unique_ptr<any_data_base> clone() const;
-        virtual void* data() noexcept;
-        virtual const void* data() const noexcept;
+        virtual const std::type_info& type() const noexcept = 0;
+        virtual std::unique_ptr<any_data_base> clone() const = 0;
+        virtual void* data() noexcept = 0;
+        virtual const void* data() const noexcept = 0;
     };
 
     std::unique_ptr<any_data_base> data;
